6754cb58039c3a4ada719eb5107a2ba16f32ee9b
Jacob Espling <jespling@calpoly.edu>
2018-09-21 16:18:22 -0700

    passed test

--- planets.py
+++ planets.py
@@ -1,10 +1,31 @@
 def weight_on_planets():
    # write your code here
    
-   print("hello world ", end=' ')
+   #establish weight on earth and store value
+   earthWeight = input("What do you weigh on earth? ")
+   
+   #convert earthweight to int
+   weightStandard = int(earthWeight)
+   
+   #calculate and store other weights on Mars and Jupiter
+   marsWeight = 0.38 * weightStandard
+   jupiterWeight = 2.34 * weightStandard
+   
+   print(f"\nOn Mars you would weigh {marsWeight} pounds.\nOn Jupiter you would weigh {jupiterWeight} pounds.")
    
    
    
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+ 
    
 if __name__ == '__main__':
    weight_on_planets()
